2917418f74a48ccde0264b2af016913d5a41d785
kwod <kwd@gmx.de>
2022-07-08 10:06:32 +0200

    my first commit

--- src/BitReals.jl
+++ src/BitReals.jl
@@ -1,5 +1,23 @@
 module BitReals
 
-# Write your package code here.
+import Base: iterate, eltype, IteratorSize
+
+export BitReals
+
+struct BitReal
+    iterablebits
+end
+
+function Base.iterate(br::BitReal; itr = br.iterablebits)
+    isnothing(itr) ? nothing : Iterators.peel(itr)
+end
+
+function Base.eltype(::Type{BitReal})
+    Bool
+end
+
+function Base.IteratorSize(::Type{BitReal})
+    Base.SizeUnknown()
+end
 
 end
